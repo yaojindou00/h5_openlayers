19426efe08b9e47f3a10e65b0907c2955670c0c2
yaojindou00 <2864043714@qq.com>
2021-12-23 11:32:26 +0800

    1.0

---
+++ static/config/config.js
@@ -0,0 +1,17 @@
+export const layers=[
+	{title:"企业图层","checked":true,
+		items:[
+			{img:"/static/img/mapswtich/qycg.png",title:"风险源企业","id":"sx_fxy","checked":false},
+			{img:"/static/img/mapswtich/qyfjq.png",title:"重金属排放企业","id":"sx_zdqy","checked":false}
+		]
+	},
+	
+	{title:"其他图层","checked":false,
+		items:[
+			{img:"/static/img/mapswtich/qyjd.png",title:"化工工业园区",id:"sx_gyyq","checked":false},
+			{img:"/static/img/mapswtich/zyqy.png",title:"尾矿",id:"sx_wk","checked":false},
+			{img:"/static/img/mapswtich/zyxl.png",title:"危化品运输线路",id:"sx_whpyslx",checked:false}
+		]
+	},
+	
+]
